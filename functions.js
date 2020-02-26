faac27cbe7ef0468613b1edcf99a1b4a7d6b9a6d
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-02-26 23:36:48 +0000

    completed preping inputs and calculations

--- functions.js
+++ functions.js
@@ -3,6 +3,11 @@ function getPercentage(input) {
     return input / 100
 }
 
+//Generate monthly repayment figure
+function generateMRepayment(estimatedSalary, percentagePerMonth) {
+    return (estimatedSalary * percentagePerMonth) / 12;
+}
+
 //Add fees if loanAmount greater than 80% (6400) and again if greater than 90% (7200)
 function feeRequiredCheck(loanRequest) {
     let validatedLoanRequest;
@@ -21,7 +26,22 @@ function upFrontFee(totalBorrowed) {
     return totalBorrowed * 0.05
 }
 
-//Add fees together for display total fees
-function totalFee(loanRequest, ) {
-
+//Generate fees together for display total fees
+function generateTotalFee(loanRequest, upFrontAdminFee) {
+    let loanIncrease, totalFeeReturned;
+    if (loanRequest >= 6400 && loanRequest < 7200) {
+        loanIncrease = 500
+    } else if (loanRequest >= 7200) {
+        loanIncrease = 1000
+    } else {
+        loanIncrease = 0
+    }
+    totalFeeReturned = upFrontAdminFee + loanIncrease;
+    return totalFeeReturned
 }
+
+//Generate total months it will take to pay, round up to highest every time
+function generateTotalMonths(totalLoanRequested, monthlyRepayment) {
+    return Math.ceil(totalLoanRequested / monthlyRepayment)
+}
+
--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-
 const loanASlider = document.getElementById('loan-amount-slider');
 const loanAOutput = document.getElementById('loan-amount-text');
-
 const expectedSSlider = document.getElementById('expected-salary-slider');
 const expectedSOutput = document.getElementById('expected-salary-text');
-
 const ppmSlider = document.getElementById('ppm-slider');
 const ppmOutput = document.getElementById('ppm-text');
-
-const calculatorSubmit = document.getElementById('calc-submit');
-
 
 //Loan amount display
 loanAOutput.innerText = loanASlider.value;
@@ -30,22 +24,27 @@ ppmSlider.oninput = function() {
 };
 
 //Calc process
-document.getElementById('form').addEventListener('submit', function(e) {
+document.getElementById('form').addEventListener('submit', (e) => {
     e.preventDefault()
+    //Grab inputs from range slider
     const loanInputValue = parseInt(loanASlider.value);
     const expectedInputValue = parseInt(expectedSSlider.value);
     const ppmInputValue = getPercentage(parseInt(ppmSlider.value));
 
-    var check = feeRequiredCheck(loanInputValue);
-    console.log(check)
+    //Monthly repayments
+    var monthlyRepayments = generateMRepayment(expectedInputValue, ppmInputValue);
 
-    //Total Borrowed
+    //Total borrowed
+    var totalLoanRequested = feeRequiredCheck(loanInputValue);
 
+    //Upfront admin fee
+    var upFrontAdminFee = upFrontFee(totalLoanRequested);
 
-    //Total Fees
-
+    //Total fees
+    var totalFee = generateTotalFee(loanInputValue, upFrontAdminFee);
 
     //Total months
+    var totalMonths = generateTotalMonths(totalLoanRequested, monthlyRepayments);
 
 });
 
