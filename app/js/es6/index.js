3883352ce0c1b74bbaf1e86a3ccde0e6a33ceb54
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-03-05 20:55:59 +0000

    amended value mins for expected salary in html and the custom exp salary event lisenter

--- app/js/es6/index.js
+++ app/js/es6/index.js
@@ -182,8 +182,8 @@ document.getElementById('customESBtn').addEventListener('click', function() {
     let customInputES = document.getElementById('customESInput').value;
     expectedSSlider.value = customInputES;
 
-    if(customInputES > 120000 || customInputES < 12000 || '') {
-        document.getElementById('errorES').innerHTML = '<p>Error! Please select an estimated salary between £12k and £120k</p>';
+    if(customInputES > 120000 || customInputES < 1 || '') {
+        document.getElementById('errorES').innerHTML = '<p>Error! Please select an estimated salary between £1 and £120k</p>';
         errorES = true;
     } else if (customInputES <= 120000){
         errorES = false;
